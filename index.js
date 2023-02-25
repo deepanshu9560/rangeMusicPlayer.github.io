d644a2f879c2b4bd5f928a1244e862bbb2478a78
deepanshu9560 <73239611+deepanshu9560@users.noreply.github.com>
2023-02-25 12:20:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,57 +12,57 @@ let rangeBar = document.querySelector('.rangeBar');
 
 let arr = [
     {
-        musicName : "All Black",
-        artistName : "Raftaar and Sukh-E"
+        musicName: "All Black",
+        artistName: "Raftaar and Sukh-E"
     },
     {
-        musicName : "Daaru Party",
-        artistName : "Millind Gaba"
+        musicName: "Daaru Party",
+        artistName: "Millind Gaba"
     },
     {
-        musicName : "She Dont Know",
-        artistName : "Millind Gaba"
+        musicName: "She Dont Know",
+        artistName: "Millind Gaba"
     }
 ];
 
 
 let isPlayed = false;
 
-const playMusic = () =>{
-    isPlayed = true;    
-    play.classList.replace('fa-play','fa-pause');
+const playMusic = () => {
+    isPlayed = true;
+    play.classList.replace('fa-play', 'fa-pause');
     music.play();
     circle.classList.add('start');
 }
 
-const pauseMusic = () =>{
+const pauseMusic = () => {
     isPlayed = false;
-    play.classList.replace('fa-pause','fa-play');
+    play.classList.replace('fa-pause', 'fa-play');
     music.pause();
     circle.classList.remove('start');
 }
 
-play.onclick = function(){   
+play.onclick = function () {
     isPlayed ? pauseMusic() : playMusic();
 }
 
-function songs(){
+function songs() {
     sName.innerHTML = arr[count].musicName;
     artistName.innerHTML = arr[count].artistName;
-    let musicConcat = arr[count].musicName;   
-    let musicNam = musicConcat.replaceAll(" ","_");    
-    music.src = musicNam+".mp3";
-    img.src = musicNam+".jpg";
-    play.classList.replace('fa-play','fa-pause');    
+    let musicConcat = arr[count].musicName;
+    let musicNam = musicConcat.replaceAll(" ", "_");
+    music.src = musicNam + ".mp3";
+    img.src = musicNam + ".jpg";
+    play.classList.replace('fa-play', 'fa-pause');
     music.play();
     isPlayed = true;
-    circle.classList.add('start');    
+    circle.classList.add('start');
 }
 
 let count = 0;
 const nextFun = () => {
     count += 1;
-    if(count == arr.length){
+    if (count == arr.length) {
         count = 0;
     }
     songs();
@@ -70,37 +70,42 @@ const nextFun = () => {
 
 const prevFun = () => {
     count -= 1;
-    if(count < 0){
-        count = arr.length-1;
+    if (count < 0) {
+        count = arr.length - 1;
     }
     // console.log(count)
     songs();
 }
 
 // making range bar
-  
-    music.addEventListener('timeupdate',(event)=>{
-        const {currentTime,duration} = event.srcElement;
-        let sec = parseInt((currentTime / duration) * 100);
-        // console.log(sec)
-        
-        rangeInp.value = sec;
-        
-        rangeBar.style.width = sec+"%"; 
-        rangeBar.style.borderRadius = '0%'; 
-        if(sec < 2){
-            rangeBar.style.width = "2%";  
-            rangeBar.style.borderRadius = '50%';         
-        }    
-    })
 
-    rangeInp.addEventListener('change',()=>{
-        console.log(rangeInp.value * music.duration/100)
-          music.currentTime = rangeInp.value * music.duration/100;
-          rangeBar.style.width = music.currentTime+"%";    
-    })
+music.addEventListener('timeupdate', (event) => {
+    const { currentTime, duration } = event.srcElement;
+    let sec = parseInt((currentTime / duration) * 100);
+    // console.log(sec)
+
+    rangeInp.value = sec;
+
+    rangeBar.style.width = sec + "%";
+    rangeBar.style.borderRadius = '0%';
+    if (sec < 2) {
+        rangeBar.style.width = "2%";
+        rangeBar.style.borderRadius = '50%';
+    }
+})
+
+rangeInp.addEventListener('change', () => {
+    console.log(rangeInp.value * music.duration / 100)
+    music.currentTime = rangeInp.value * music.duration / 100;
+    rangeBar.style.width = music.currentTime + "%";
+})
+
+// ended function call
+music.addEventListener('ended', () => {
+    nextFun();
+});
 
 
 
-next.addEventListener('click' , nextFun);
-prev.addEventListener('click' , prevFun);
+next.addEventListener('click', nextFun);
+prev.addEventListener('click', prevFun);
